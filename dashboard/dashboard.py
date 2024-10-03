6ef22888723ff408fd740fe587eed70ba927a905
Jihan Sabilla Kamal <jihansabilla49@gmail.com>
2024-10-03 11:48:37 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -42,7 +42,7 @@ def create_rfm_df(df):
     return rfm_df
 
 # Load cleaned data
-all_df = pd.read_csv("all_data.csv")
+all_df = pd.read_csv("dashboard/all_data.csv")
 
 all_df.sort_values(by="dteday", inplace=True)
 all_df.reset_index(inplace=True)
